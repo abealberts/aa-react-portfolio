eabdcbfe087a4a33abc7feeb9935cb71897ce2e5
Abe <abe.wav.bis@gmail.com>
2024-02-09 02:58:44 -0600

    Updated about page with placeholder content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,6 +3,21 @@ export default function AboutPage() {
       <div className="container">
         <div className="content">
             <h2>About Me</h2>
+            <div className="card pt-4">
+              <div className="card-image">
+                <figure className="image is-4by3 mt-4">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  Hi there! My name is Abe and I am a full-stack web developer. I build apps using the MERN stack. I have a strong creative background which helps with not only visual design aspects of projects, but the creative problem solving and engineering as well.
+                  <br/>
+                  <br/>
+                  Check out some of my recent projects in my <a href="/projects">portfolio</a>, or <a href="/contact">get in touch!</a>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     );
