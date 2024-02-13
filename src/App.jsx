986e331bf58a386ed75afa68b084a369f7effb58
Abe <abe.wav.bis@gmail.com>
2024-02-12 19:39:21 -0600

    Reorganized components to meet acceptance criteria

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
         <Header />
-        <Nav />
             {/* <div className='is-fullheight'> */}
               <Outlet />
             {/* </div> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
+import Nav from './Nav';
+
 export default function Header() {
     return (
-        <div className='navbar'>
-            <div className="navbar-brand">
-                <figure className="image is-64x64 p-2">
-                    <img src="../header-logo.svg" height="64" width="64"/>
-                </figure>
-                <p className='navbar-item'><strong>Abe Alberts /</strong>/ Fullstack Web Developer</p>
+        <div className='container mb-4'>
+            <div className='navbar'>
+                <div className="navbar-brand">
+                    <figure className="image is-64x64 p-2">
+                        <img src="../header-logo.svg" height="64" width="64"/>
+                    </figure>
+                    <p className='navbar-item'><strong>Abe Alberts /</strong>/ Fullstack Web Developer</p>
+                </div>
             </div>
+            <Nav />
         </div>
+
     );
 }
